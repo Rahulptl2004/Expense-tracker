9050da779eb49deccf9503bdabc8868d88561da3
Rahul Patel <rahulptl2004@gmail.com>
2026-09-10 10:03:28 +0530

    authorization

---
+++ server/middleware/authMiddlerware.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) {
+            return res.status(401).json({
+                message: "No token provided",
+            });
+        }
+        const token = authHeader.startsWith("Bearer ")
+            ? authHeader.split(" ")[1]
+            : authHeader;
+        
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+         console.log("JWT ERROR:", error.message);
+
+        return res.status(401).json({
+            message: "Invalid or expire token"
+        });
+    }
+};
+
+export default authMiddleware
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
+import authMiddleware from "../middleware/authMiddlerware.js";
 
 const router = express.Router();
 
@@ -8,5 +9,12 @@ router.post("/register", registerUser);
 
 // Login User
 router.post("/login", loginUser);
+
+router.get("/profile", authMiddleware, (req, res) => {
+    res.json({
+        message: "Protected route accessed",
+        user: req.user,
+    });
+});
 
 export default router;
